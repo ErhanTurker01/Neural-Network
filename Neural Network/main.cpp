5a912ddf7c24f7e25ddff39475458022c93181ef
ErhanTurker01 <erantrk0122@gmail.com>
2024-11-02 21:39:55 +0300

    Initial Commit

---
+++ Neural Network/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Neural Network
+//
+//  Created by Erhan Türker on 11/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
